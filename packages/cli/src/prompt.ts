d95d56be0044adb32a366f6d7b92d945fd620a35
Miguel Ramos <miguel.marques.ramos@gmail.com>
2021-12-15 14:00:54 +0000

    feat: initial configs setup (#3)
    Co-authored-by: Miguel Ramos <miguel.marques.ramos@gmail.com>

--- packages/cli/src/prompt.ts
+++ packages/cli/src/prompt.ts
@@ -1,3 +1,10 @@
+/**
+ * Copyright Websublime All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://websublime.dev/license
+ */
+
 import prompts from 'prompts';
 
 const OperationCancel = () => {
